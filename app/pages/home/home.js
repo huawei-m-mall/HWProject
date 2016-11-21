55e96fe4fd42bd719cd1c100fcf073a1a47bd3cd
jmc <935453286@qq.com>
2016-11-21 19:34:10 +0800

    第二次提交

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -6,7 +6,6 @@ angular.module('huaweiMall.homePage',[])
 	.state({
 		name:'home',
 		url:'/home',
-		// css:'app/pages/home/swiper-3.3.1.min.css',
 		css:'app/pages/home/home.css',
 		templateUrl:'app/pages/home/home.html',
 		controller:'homeCtrl'
@@ -19,13 +18,31 @@ angular.module('huaweiMall.homePage',[])
 //		controller:function($css){
 //			$css.add('app/pages/home/home.css');
 //		}
+})
+	.state({
+		name:'phoneNav',
+		url:'/phoneNav',
+		css:'app/pages/home/phoneNav/phoneNav.css',
+		templateUrl:'app/pages/home/phoneNav/phoneNav.html'
 	})
-//	
-//	.state({
-//		name:'home.part2',
-//		url:'/part2',
-//		templateUrl:'app/pages/home/home.part2/home.part2.html'
-//	})
+	.state({
+		name:'preference',
+		url:'/preference',
+		css:'app/pages/home/preference/preference.css',
+		templateUrl:'app/pages/home/preference/preference.html'
+	})
+	.state({
+		name:'v_code',
+		url:'/v_code',
+		css:'app/pages/home/v_code/v_code.css',
+		templateUrl:'app/pages/home/v_code/v_code.html'
+	})
+	.state({
+		name:'old_to_new',
+		url:'/old_to_new',
+		css:'app/pages/home/old_to_new/old_to_new.css',
+		templateUrl:'app/pages/home/old_to_new/old_to_new.html'
+	})
 	
 })
 .controller('homeCtrl',function($scope){
